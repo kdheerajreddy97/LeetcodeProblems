55d46b794f688475cc24b779a81c43ae56dbc50a
Dheeraj Reddy Kukkala <96662214+kdheerajreddy97@users.noreply.github.com>
2023-09-09 15:25:51 -0400

    Time: 62 ms (67.54%), Space: 16.5 MB (44.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,11 @@ class Solution:
         substr = set()
         l = 0
         res = 0
-        
         for r in range(len(s)):
             while s[r] in substr:
                 substr.remove(s[l])
                 l +=1
             substr.add(s[r])
-                
             res = max(res, r-l+1)
         return res
+    
