85d999f353c82c691b10edb243e74b60ba788de1
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-09-01 18:28:11 -0500

    Time: 68 ms (86.10%), Space: 20.1 MB (31.16%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,26 +1,28 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
         start = []
         end = []
-        
         for interval in intervals:
             start.append(interval[0])
             end.append(interval[1])
-        
+            
         start.sort()
         end.sort()
-        
-        s, e = 0, 0
-        count, meetings = 0, 0
-            
-        while s < len(intervals):
-
-            if start[s] < end[e]:
-                s +=1
-                count+=1
-                meetings = max(meetings, count)
+        i = 0
+        j = 0
+        res = 0
+        meetings = 0
+        while i < len(intervals):
+            if end[j] > start[i]:
+                i+=1
+                meetings+=1
             else:
-                e += 1
-                count -= 1
-        return meetings
+                meetings -=1
+                j+=1
+            res = max(res, meetings)
+        return res
+            
         
+    
+                
