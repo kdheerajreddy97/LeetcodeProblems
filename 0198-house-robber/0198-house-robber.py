4da3bf331aa462d8da5cc917f8a81e8ff3a06867
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-09-28 02:38:44 -0500

    Time: 35 ms (61.42%), Space: 16.6 MB (29.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,15 +5,15 @@ class Solution:
         
         if m == 0:
             return 0
-        if m == 1:
-            return nums[0]
+        # if m == 1:
+        #     return nums[0]
         dp = [0] * m
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        # dp[1] = max(nums[0], nums[1])
         
         
         
-        for i in range(2,m):
+        for i in range(1,m):
             dp[i] = max(nums[i] + dp[i-2], dp[i-1])
         
         
