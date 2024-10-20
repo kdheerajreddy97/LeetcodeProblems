2ec64693172d7d5d83817ed975ad307c7bd2b65e
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-10-20 01:27:03 -0500

    Time: 0 ms (100.00%), Space: 16.6 MB (41.78%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,21 +8,32 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
         q = deque([root])
+        if not root:
+            return []
         
         while q:
             rightside = None
             
-        
             for i in range(len(q)):
                 node = q.popleft()
                 
-                if node:
-                    rightside = node
-                    q.append(node.left)
-                    q.append(node.right)
+                rightside = node
                 
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
             if rightside:
                 res.append(rightside.val)
+                
+#                 if node:
+#                     rightside = node
+#                     q.append(node.left)
+#                     q.append(node.right)
+                
+#             if rightside:
+#                 res.append(rightside.val)
+
         return res
                            
                 
