adac0773a07c97b913b38eb14a4d5d467035dc49
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-10-23 20:59:42 -0500

    Time: 47 ms (97.37%), Space: 34 MB (77.68%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,16 +1,20 @@
+# More efficient than the below one as we are not creating a new list everytime
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         
         def helper(s, path):
             if not s:
-                res.append(path)
+                res.append(path[:])
                 return
             
             for i in range(1,len(s)+1):
                 partition = s[:i]
                 if ispalindrome(partition):
-                    helper(s[i:],path + [partition])
+                    #path = path + [partition]
+                    path.append(partition)
+                    helper(s[i:],path)
+                    path.pop()
                 
 
         
@@ -26,3 +30,23 @@ class Solution:
         
         helper(s, [])
         return res
+    
+# class Solution:
+#     def partition(self, s: str) -> List[List[str]]:
+#         res = []
+        
+#         def helper(s, path):
+#             if not s:
+#                 res.append(path)
+#                 return
+            
+#             for i in range(1, len(s) + 1):
+#                 partition = s[:i]
+#                 if is_palindrome(partition):
+#                     helper(s[i:], path + [partition])
+        
+#         def is_palindrome(sub):
+#             return sub == sub[::-1]  # Simple palindrome check
+        
+#         helper(s, [])
+#         return res
