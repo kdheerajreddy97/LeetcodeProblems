7ac543326984ade21cca4cabcde246101e9a1c33
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-03 22:23:02 -0600

    Time: 11 ms (63.94%), Space: 19.4 MB (75.49%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,40 +3,78 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+##Priority Queue Approach
+#Tends to be simpler to implement and may perform better in practice due to the efficient management of the minimum element with a priority queue
+#Time Complexity: O(Nlogk)
+
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists: List[Optional[ListNode]])  -> Optional[ListNode]:
+        min_heap = []
         
         if len(lists) == 0:
             return None
+        
+        for index, node in enumerate(lists):
+            if node:
+                heappush(min_heap,(node.val, index, node))
+        
+        dummy = ListNode()
+        curr = dummy
+        
+        while min_heap:
+            val, index, node = heappop(min_heap)
+            curr.next = node
+            curr = curr.next
+            
+            if node.next:
+                heappush(min_heap, (node.next.val, index, node.next))
+        return dummy.next
+            
 
-        while len(lists) > 1:
-            mergedLists  = []
-            for i in range(0, len(lists), 2):
-                list1 = lists[i]
-                list2 = lists[i+1] if i+1 < len(lists) else None
-                mergedLists.append(self.merge(list1, list2))
-            lists = mergedLists
-        return lists[0]
+
+###Divide and Conquer Approach###
+#Requires less additional memory because it does not need a priority queue, just temporary merged lists.
+#Time Complexity: O(Nlogk)
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+#         if len(lists) == 0:
+#             return None
+
+#         while len(lists) > 1:
+#             mergedLists  = []
+#             for i in range(0, len(lists), 2):
+#                 list1 = lists[i]
+#                 list2 = lists[i+1] if i+1 < len(lists) else None
+#                 mergedLists.append(self.merge(list1, list2))
+#             lists = mergedLists
+#         return lists[0]
                 
           
     
-    def merge(self, list1: Optional[ListNode], list2: Optional[ListNode] ) -> Optional[ListNode]:
-        dummy = ListNode()
-        curr = dummy
-        pi = float('inf')
+#     def merge(self, list1: Optional[ListNode], list2: Optional[ListNode] ) -> Optional[ListNode]:
+#         dummy = ListNode()
+#         curr = dummy
+#         pi = float('inf')
         
-        while list1 or list2:
-            value1 = list1.val if list1 else pi
-            value2 = list2.val if list2 else pi
+#         while list1 or list2:
+#             value1 = list1.val if list1 else pi
+#             value2 = list2.val if list2 else pi
             
-            if value1 < value2:
-                curr.next = ListNode(value1)
-                curr = curr.next
-                list1 = list1.next if list1 else None
+#             if value1 < value2:
+#                 curr.next = ListNode(value1)
+#                 curr = curr.next
+#                 list1 = list1.next if list1 else None
             
-            else:
-                curr.next = ListNode(value2)
-                curr = curr.next
-                list2 = list2.next if list2 else None
-        return dummy.next
+#             else:
+#                 curr.next = ListNode(value2)
+#                 curr = curr.next
+#                 list2 = list2.next if list2 else None
+#         return dummy.next
+
+
+
+
+
             
