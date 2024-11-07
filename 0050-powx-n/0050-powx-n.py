35004f76b34793c1834ee4485e9a9167a794ce80
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-06 19:41:54 -0600

    Time: 0 ms (100.00%), Space: 16.6 MB (37.76%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,21 +1,48 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
+#     Space complexity: O(1) ; Time complexity: O(log(n))
+#     def myPow(self, x: float, n: int) -> float:
         
-        result = 1
+#         result = 1
         
-        if n < 0:
-            x = 1/x
-            n = -n
+#         if n < 0:
+#             x = 1/x
+#             n = -n
             
             
             
-        while n != 0:
-            if n % 2 != 0:
-                result = result * x
+#         while n != 0:
+#             if n % 2 != 0:
+#                 result = result * x
             
-            n = n//2
-            x = x * x
+#             n = n//2
+#             x = x * x
             
             
             
-        return result
+#         return result
+
+
+#      Recursive Approach: Space complexity: O(log(n)) ; Time complexity: O(log(n))
+        def myPow(self, x: float, n: int) -> float:
+        
+            if n == 0:
+                return 1
+            
+            if n < 0:
+                x = 1/x
+                n = -n
+            
+            result = self.myPow(x,n//2)
+            
+
+                
+            
+            if n % 2 == 0:
+                return result * result
+            else:
+                return result * result * x
+                
+        
+        
+        
+        
