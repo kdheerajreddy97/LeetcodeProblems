24b78f89b7bebc23ca4991797399b753e6ea1bd2
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-06 19:25:18 -0600

    Time: 0 ms (100.00%), Space: 16.6 MB (72.91%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,21 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        result = 1
+        
+        if n < 0:
+            x = 1/x
+            n = -n
+            
+            
+            
+        while n != 0:
+            if n % 2 != 0:
+                result = result * x
+            
+            n = n//2
+            x = x * x
+            
+            
+            
+        return result
