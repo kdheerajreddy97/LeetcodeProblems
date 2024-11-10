9c17065f19a6e6c60fa60b2283fd95ae950382d1
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-10 11:59:04 -0600

    Time: 2 ms (39.63%), Space: 20.1 MB (33.04%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,27 +4,50 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Brute Force BFS and sort the array to get the k-th largest
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+#         q = deque()
+#         q.append(root)
+        
+#         res = []
+#         res.append(root.val)
+        
+#         while q:
+            
+#             for i in range(len(q)):
+#                 node = q.popleft() 
+#                 if node.left:
+#                     res.append(node.left.val)
+#                     q.append(node.left)
+                
+#                 if node.right:
+#                     res.append(node.right.val)
+#                     q.append(node.right)
+                
+        
+#         res.sort()
+#         return res[k-1]
+    
+#Inorder traversal and find the kth element
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        q = deque()
-        q.append(root)
-        
         res = []
-        res.append(root.val)
-        
-        while q:
+        def dfs(node):
+            if node == None:
+                return
             
-            for i in range(len(q)):
-                node = q.popleft() 
-                if node.left:
-                    res.append(node.left.val)
-                    q.append(node.left)
-                
-                if node.right:
-                    res.append(node.right.val)
-                    q.append(node.right)
-                
+            if node:
+                dfs(node.left)
+            res.append(node.val)
+            if node:
+                dfs(node.right)
+            # if node:
+            #     return dfs(node.left) + [node.val] + dfs(node.right)
+            # else:
+            #     return []
         
-        res.sort()
+        dfs(root)
+        
         return res[k-1]
