c53fc52b6cf4e14b41ac0e5c45bef220768fa00e
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-19 02:39:59 -0600

    Time: 75 ms (49.80%), Space: 20 MB (56.67%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,40 +1,80 @@
-#Can be done in two ways: Either use stack or take 4 variables and follow appraoch similar to expression add operators
+#Can be done in two ways: Either use stack or take 4 variables and follow approach similar to expression add operators
+# class Solution:
+#     def calculate(self, s: str) -> int:
+        
+#         currval = 0
+#         lastsign = '+'
+#         calcval = 0
+#         tail = 0
+        
+        
+        
+#         for i in range(len(s)):
+#             c = s[i]
+            
+#             #check if digit
+#             if c.isdigit():
+#                 currval = currval * 10 + int(c)
+            
+#             if ((not c.isdigit()) and (c != ' ')) or i == len(s) - 1:
+                
+#                 if lastsign == '+':
+#                     calcval += currval
+#                     tail = currval
+#                 elif lastsign == '-':
+#                     calcval -= currval
+#                     tail = -currval
+#                 elif lastsign == '*':
+#                     calcval = (calcval - tail) + (tail * currval)
+#                     tail = tail * currval
+#                 elif lastsign == '/':
+#                     calcval = (calcval - tail) + int (tail / currval)
+#                     tail = int(tail / currval)
+                    
+                    
+#                 currval = 0
+#                 lastsign = c
+        
+#         return calcval
+
+
+
+#Using Stack
+
 class Solution:
     def calculate(self, s: str) -> int:
         
         currval = 0
         lastsign = '+'
-        calcval = 0
-        tail = 0
         
-        
-        
+        mystack = []
+        res = 0
+    
         for i in range(len(s)):
             c = s[i]
             
             #check if digit
             if c.isdigit():
                 currval = currval * 10 + int(c)
-                print(currval)
             
             if ((not c.isdigit()) and (c != ' ')) or i == len(s) - 1:
                 
                 if lastsign == '+':
-                    calcval += currval
-                    tail = currval
+                    mystack.append(currval)
                 elif lastsign == '-':
-                    calcval -= currval
-                    tail = -currval
-                    print(calcval)
+                    mystack.append(-currval)
                 elif lastsign == '*':
-                    calcval = (calcval - tail) + (tail * currval)
-                    tail = tail * currval
+                    temp = mystack.pop()
+                    mystack.append(temp * currval)
                 elif lastsign == '/':
-                    calcval = (calcval - tail) + int (tail / currval)
-                    tail = int(tail / currval)
+                    temp = mystack.pop()
+                    mystack.append(int(temp / currval))
                     
                     
                 currval = 0
                 lastsign = c
+                
+        for i in range(len(mystack)):
+            res += mystack.pop()
         
-        return calcval
+        return res
