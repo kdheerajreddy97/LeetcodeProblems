3665b0daa3775d8bb439ff0ea85a1de0981f7fc1
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-19 03:51:25 -0600

    Time: 38 ms (42.09%), Space: 17.4 MB (67.33%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,47 @@
+class Solution:
+    
+    def __init__(self):
+        self.idx = 0
+    
+    def calculate(self, s: str) -> int:
+        
+        currval = 0
+        lastsign = '+'
+        calcval = 0
+        tail = 0
+        n = len(s)
+        
+        
+        while self.idx < n:
+            c = s[self.idx]
+            self.idx += 1
+            
+            #check if digit
+            if c.isdigit():
+                currval = currval * 10 + int(c)
+                
+            elif c == '(':
+                currval = self.calculate(s)
+            
+            if ((not c.isdigit()) and (c != ' ')) or self.idx == n:
+                
+                if lastsign == '+':
+                    calcval += currval
+                    tail = currval
+                elif lastsign == '-':
+                    calcval -= currval
+                    tail = -currval
+                elif lastsign == '*':
+                    calcval = (calcval - tail) + (tail * currval)
+                    tail = tail * currval
+                elif lastsign == '/':
+                    calcval = (calcval - tail) + int (tail / currval)
+                    tail = int(tail / currval)
+                    
+                if c == ')':
+                    return calcval
+                
+                currval = 0
+                lastsign = c
+        
+        return calcval
