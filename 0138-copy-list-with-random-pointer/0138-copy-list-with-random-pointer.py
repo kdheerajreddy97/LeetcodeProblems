26a9fd9bb0353b87d1cefcb2635c9ff873d8609e
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-20 01:21:25 -0600

    Time: 35 ms (74.86%), Space: 17.5 MB (62.14%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,40 @@
+# Approach 1: Two Pass Time:O(2n)  Space: O(n) (HashMap)
+# Approach 2: Single Pass Time:O(n)  Space: O(n) (HashMap)
+# Approach 3: Single Pass Time:O(n)  Space: O(1) (No HashMap)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+# Two Pass
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if not head:
+            return None
+        dict = {}
+        
+        curr = head
+        
+        while curr:
+            dict[curr] = Node(curr.val)
+            curr = curr.next
+            
+        curr = head
+        
+        while curr:
+            if curr.next:
+                dict[curr].next = dict[curr.next]
+            
+            if curr.random:
+                dict[curr].random = dict[curr.random]
+            
+            curr = curr.next
+        
+        return dict[head]
+        
+        
+        
