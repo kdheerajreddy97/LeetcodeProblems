f9c835640bf5e9f1d9808401bc1519172b6689d6
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-22 02:28:32 -0600

    Time: 0 ms (100.00%), Space: 22.1 MB (88.00%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,11 +1,30 @@
 # Can be done in O(n) or O(log(n)) - Binary Search
-# Bianry Search
+# Single Pass
+# class Solution:
+#     def hIndex(self, citations: List[int]) -> int:
+#         n = len(citations)
+        
+#         for i in range(n):
+            
+#             if citations[i] >= (n - i):
+#                 return n-i
+#         return 0
+    
+
+# Binary Search
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        l = 0
+        r = len(citations) - 1
         n = len(citations)
-        
-        for i in range(n):
+        h_max = 0
+        while l <= r:
+            mid = (l + r) // 2
+            h = n - mid
+            if citations[mid] >= h:
+                r = mid - 1
+                h_max = max(h_max, h)
+            else:
+                l = mid + 1
             
-            if citations[i] >= (n - i):
-                return n-i
-        return 0
+        return h_max
