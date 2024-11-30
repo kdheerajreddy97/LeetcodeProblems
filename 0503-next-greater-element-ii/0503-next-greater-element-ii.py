76ca47e34051b3b82672116061ea9ce6c619eb6e
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-11-29 19:20:27 -0600

    Time: 27 ms (47.96%), Space: 19.3 MB (6.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,5 +1,6 @@
 # Similar to daily Temperatures question but it since its circular we need to iterate over it twice to find next greater number
-# 
+# Dont forget to skip the number (i.e. appending to stack) that have already been added before
+# Time: O(2n) Space: O(n) + O(n)
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -12,8 +13,11 @@ class Solution:
                 temp = mystack.pop()
                 next_greater = nums[i%n]
                 res[temp] = next_greater
-            
-            if res[i%n] == -1:
+            # Append to stack only if it's the first iteration or if the element hasn't been processed
+            if i < n:
                 mystack.append(i%n)
+            else:
+                if mystack[-1] == i%n:
+                    break
             
         return res
