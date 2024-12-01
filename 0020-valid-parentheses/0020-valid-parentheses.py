27aca31354b8ae6e10ce87f122b66e475db82a9e
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-12-01 03:02:28 -0600

    Time: 3 ms (24.64%), Space: 17.5 MB (5.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,3 +1,5 @@
+# Time O(n); Space: O(n)
+# Just add into the Stack if its a open braces and check if the next character is equal to the top of the stack (else condition)
 class Solution:
     def isValid(self, s: str) -> bool:
         mystack = []
@@ -11,10 +13,12 @@ class Solution:
                 elif i == '[':
                     mystack.append(']')
             else:
+                # Edge case if it starts with closed braces then stack will be empty so return False
                 if not mystack or i != mystack[-1]:
                     return False
                 else:
                     mystack.pop()
+        # Make sure to return true only of if the stack is empty
         return len(mystack) == 0
                 
         
