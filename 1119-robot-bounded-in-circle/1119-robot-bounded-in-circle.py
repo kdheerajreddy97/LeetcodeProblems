9c360544ddc5795e24619970f97370bf05578296
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-12-21 01:05:16 -0600

    Time: 0 ms (100%), Space: 18 MB (6.15%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
@@ -1,6 +1,7 @@
 # 2 Conditions to be successfull
 # 1. If after one cycle if it faces north it always faces north(Can be any direction N|S|E|W) - fails always - so it works when it doesn't point to same direction
 # 2. After once cycle if its visits origin again then its True
+# Time: O(n); Space: O(1)
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
                     # North, East, South, West
@@ -9,17 +10,20 @@ class Solution:
         x = 0
         y = 0
 
-
         for step in instructions:
+            # If 'G' - add the current pointer value to current x and y
             if step == 'G':
                dr, dc = directions[idx]
                x += dr
                y += dc
+            # 3 steps clockwise rotation
             elif step == 'L':
                 idx = (idx + 3) % 4
+            # 1 step clockwise rotation
             else:
                 idx = (idx + 1) % 4
-            
+        # Condition 1 and 2 as discussed in the top
+        # idx != 0 - This is because at any of 4 iterations from here it will reach a cycle
         if (x == 0 and y == 0) or idx != 0:
             return True
         else:
