6171d9838e19ba889960b93df10f9db46aa4b51f
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-12-29 07:07:38 -0600

    Time: 14 ms (12.58%), Space: 24.3 MB (5.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,18 @@
+# 2 Approaches: Bucket Sort or Heaps; For both we need to store the frequency of each element in dictionary
+# Approach 1: Bucket Sort
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Space: O(n)
         dict = {}
+        # Time: O(n)
         for num in nums:
             if num in dict:
                 dict[num] += 1
             else:
                 dict[num] = 1
-        
+        # Space: O(n); Time: O(n); Maximum an element can repeat is "len(nums)" times
         bucket = [[] for _ in range(len(nums) + 1)]
-        
+        # Time: O(n)
         for key, val in dict.items():
             bucket[val].append(key)
         
