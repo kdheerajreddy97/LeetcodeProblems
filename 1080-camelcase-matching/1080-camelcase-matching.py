f78b8140c88899bd30b723fafe46a0c96bb71f75
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-01-01 13:37:20 -0600

    Time: 0 ms (100%), Space: 17.8 MB (12.42%) - LeetHub

--- 1080-camelcase-matching/1080-camelcase-matching.py
+++ 1080-camelcase-matching/1080-camelcase-matching.py
@@ -1,3 +1,4 @@
+# Time: N O(m + n)
 class Solution:
     def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
         res = []
@@ -8,7 +9,7 @@ class Solution:
                 res.append(False)
 
         return res
-
+    # 2 Pointers approach
     def matching(self, query, pattern):
         i = 0
         j = 0
@@ -19,13 +20,16 @@ class Solution:
             if query[i] == pattern[j]:
                 i += 1
                 j += 1
+            # No Match
             else:
+                # If uppercase then return False - Ex: "FrameBuffeR"
                 if query[i].isupper():
                     return False
                 i += 1
         if i > m:
             return False
         if j == n and i <= m:
+            # Check if the remaining string contains the UpperCase letters Ex: "FooBarTest"
             while i < m:
                 if query[i].isupper():
                     return False
