c339d1bcd5ea4d9ddd91fc8283b42c19d610daeb
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-01-04 03:27:15 -0600

    Time: 214 ms (36.65%), Space: 48.9 MB (17.61%) - LeetHub

--- 1097-stream-of-characters/1097-stream-of-characters.py
+++ 1097-stream-of-characters/1097-stream-of-characters.py
@@ -12,43 +12,37 @@ class Trie:
     
     def insert(self, word):
         node = self.root 
-        for c in word[::-1]:  # Insert the reversed word
+        for c in word[::-1]:
             pos = ord(c) - ord('a')
             if not node.children[pos]:
                 node.children[pos] = TrieNode()
             node = node.children[pos]
-        node.startsWith = True  # Mark the end of the word
+        node.startsWith = True
     
 class StreamChecker:
 
     def __init__(self, words: List[str]):
         self.trie = Trie()
-        self.stream = []  # To store the stream of queried letters
-        self.max_length = 0  # Keep track of the maximum word length
-        
-        # Insert all words into the Trie and track the maximum word length
+        self.result = []
+        self.max_length = 0
         for word in words:
             self.trie.insert(word)
             self.max_length = max(self.max_length, len(word))
 
     def query(self, letter: str) -> bool:
-        self.stream.append(letter)
-        
-        # Limit the stream size to the maximum word length for efficiency
-        if len(self.stream) > self.max_length:
-            self.stream.pop(0)
-
-        # Start searching from the root of the Trie
         curr = self.trie.root
-        for c in reversed(self.stream):  # Check prefixes in reverse order
+        self.result.append(letter)
+        if len(self.result) > self.max_length:
+            self.result.pop(0)
+
+        for c in self.result[::-1]:
             pos = ord(c) - ord('a')
             if not curr.children[pos]:
                 return False
             curr = curr.children[pos]
             if curr.startsWith:
-                return True  # Found a valid prefix
+                return True
         return False
-
 
         
 
