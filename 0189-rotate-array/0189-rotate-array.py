c6d7e34cb48baa20f498323eca26e108b836969a
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-01-18 12:02:10 -0600

    Time: 7 ms (46.53%), Space: 25.5 MB (42.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,40 +1,34 @@
-# 1. Using Extra Array - Single Pass
-# class Solution:
-#     def rotate(self, nums: List[int], k: int) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         n = len(nums)
-#         out = [0] * n
-#         for i in range(n):
-#             if i < n-k:
-#                 out[k+i] = nums[i] 
-#             else:
-#                 out[i-n+k] = nums[i]
-#         return out
-                
-# 2. 2 Pass- O(1)
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
         n = len(nums)
-        #Edge case - Dont forget
-        k = k%n
-        l = 0
-        r = n - 1
-        
-        self.reverse(nums, 0, n-1)
-        self.reverse(nums, 0, k-1)
-        self.reverse(nums, k, n-1)
-            
-            
-    def reverse(self,nums, l, r):
+        k = k % n
+        if n ==1:
+            return nums
+        self.reverse(0, n, nums)
+        self.reverse(0,k,nums)
+        self.reverse(k, n, nums)
+
+    def reverse(self, i,j, nums):
+        l = i
+        r = j-1
         while l < r:
-            self.swap(nums, l, r)
+            temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
             l += 1
             r -= 1
-    
-    def swap(self,nums, i ,j):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
-        
+
+
+    # def swap(self, arr):
+    #     l = 0
+    #     r = len(arr)-1
+    #     while l < r:
+    #         temp = arr[l]
+    #         arr[l] = arr[r]
+    #         arr[r] = temp
+    #         l += 1
+    #         r -= 1
