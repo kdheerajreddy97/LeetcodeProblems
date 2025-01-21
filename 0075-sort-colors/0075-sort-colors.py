1ca4fb43ab245bb091f72f6a2416b10b41aaea3e
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-01-21 08:59:23 -0600

    Time: 0 ms (100%), Space: 17.6 MB (51.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,21 +1,28 @@
+# Approach take 3 pointers; low = , mid =0, high = n - if mid =0; swap with left and increment both; if 1 increment; else decrement high
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums) == 1:
-            return nums
-        dict = {}
-        dict[0] = 0
-        dict[1] = 0
-        dict[2] = 0
-        for i in range(len(nums)):
-            dict[nums[i]] += 1
+        low = 0
+        high = len(nums) -1
+        mid = 0
         
-        for i in range(dict[0]):
-            nums[i] = 0
-        for i in range(dict[0],dict[0]+ dict[1]):
-            nums[i] = 1
-        for i in range(dict[0]+ dict[1], dict[0]+ dict[1]+ dict[2]):
-            nums[i] = 2
+        while (mid <= high):
+            if nums[mid] == 0:
+                temp = nums[low]
+                nums[low] = nums[mid]
+                nums[mid] = temp
+                
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                temp = nums[high]
+                nums[high] = nums[mid]
+                nums[mid] = temp
+                
+                high -= 1
         return nums
