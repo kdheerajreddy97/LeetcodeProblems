33c229659ed9b3866c891359cb537634b0463aae
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-01-21 15:53:30 -0600

    Time: 0 ms (100%), Space: 17.7 MB (31.89%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,8 +1,8 @@
-#Brute way - O(m*n) time complexity
+#Bruteforce way - O(m*n) time complexity
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
-        for i in range(len(haystack)):
+        for i in range(len(haystack)- len(needle) + 1):
             k = i
             j = 0
             while j < len(needle) and k < len(haystack) and haystack[k] == needle[j]:
@@ -13,8 +13,10 @@ class Solution:
             
         return -1
                 
-                
-                
+
+#Rolling Has technique - Robin Karp Algorithmn
+
+#KMP algorithmn
                     
                 
         
