cacb2c003b82bfff471a0cedd65426320616e8bf
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-03-13 19:47:12 -0500

    Time: 0 ms (100%), Space: 17.9 MB (16.36%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,19 +1,16 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s =s.split(" ")
+        s = s.split(" ")
         dict1 = {}
         dict2 = {}
-        
         if len(pattern) != len(s):
             return False
-        
+
         for i in range(len(pattern)):
-            char = pattern[i]
-            word = s[i]
-            
-            if ((char in dict1) and (dict1[char] != word)) or ((word in dict2) and (dict2[word] != char)):
+            p1 = pattern[i]
+            s1 = s[i]
+            if (p1 in dict1 and dict1[p1] != s1) or (s1 in dict2 and dict2[s1]!= p1):
                 return False
-            else:
-                dict1[char] = word
-                dict2[word] = char
+            dict1[p1] = s1
+            dict2[s1] = p1
         return True
