cc70b55fc0449e91b8d95e5771ecf03f9730ea64
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-03-17 22:48:59 -0500

    Time: 88 ms (32.9%), Space: 32.9 MB (23.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,33 +1,12 @@
-# class Solution:
-#     def maxSubArray(self, nums: List[int]) -> int:
-#         l, r = 0,0
-#         max_sum = nums[0]
-#         current_sum = 0
-        
-#         for n in nums:
-            
-#             if current_sum < 0:
-#                 current_sum = 0
-#             current_sum += n
-#             max_sum = max(max_sum, current_sum)
-#         return max_sum
-        
-# Brute Force takes O(n2)
-# This approach does it in 1 pass: O(n)
-# Calcualte currentsum everytime and take the max of currentsum and current pointer
-# Take another variable and take max of above value every time pointer increments
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
         curr_sum = nums[0]
         max_sum = nums[0]
-        
-        for i in range(1,len(nums)):
-            curr_sum = max(nums[i], curr_sum + nums[i])
+        for i in range(1, n):
+            curr_sum += nums[i]
+            curr_sum = max(curr_sum, nums[i])
             max_sum = max(max_sum, curr_sum)
-            
         return max_sum
-    
-# Follow up Question: If we need to return the maximum subarray: Then try to do it 3 pointers
-            
-            
+ 
             
