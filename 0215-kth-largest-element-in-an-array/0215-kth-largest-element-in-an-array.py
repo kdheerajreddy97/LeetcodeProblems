5a6782686d26abc5e8c06ded7d601c0fb1586c61
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-03-25 19:42:11 -0500

    Time: 75 ms (63.45%), Space: 28.9 MB (26.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,44 +1,14 @@
 #Time complexity: NLog(k)
+import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int ) -> int:
         min_heap = []
-        for i, val in enumerate(nums):
+        
+        for i in range(len(nums)):
             if len(min_heap) < k:
-                heappush(min_heap,val)
-            
-            elif val > min_heap[0]:
-                heappop(min_heap)
-                heappush(min_heap, val)
-        return heappop(min_heap)
+                heapq.heappush(min_heap, nums[i])
+            elif min_heap[0] <= nums[i]:
+                heapq.heappop(min_heap)
+                heapq.heappush(min_heap, nums[i])
+        return heapq.heappop(min_heap)
 
-
-
-# # Quick Select Method : invloves partition everytime more efficent that heaps
-# class Solution:
-#     def findKthLargest(self, nums: List[int], k: int) -> int:
-#         k = len(nums) - k
-        
-        
-#         def quickselect(l, r):
-#             pivot = nums[r]
-#             p = l
-            
-            
-#             for index in range(l,r):
-#                 if nums[index] <= pivot:
-#                     nums[p], nums[index] = nums[index], nums[p]
-#                     p +=1
-            
-#             nums[p], nums[r] = nums[r], nums[p]
-            
-#             if p > k: 
-#                 return quickselect(l, p-1)
-#             elif p < k:
-#                 return quickselect(p+1, r)
-#             else:
-#                 return nums[p]
-            
-#         return quickselect(0,len(nums)-1)
-                    
-                    
-        
