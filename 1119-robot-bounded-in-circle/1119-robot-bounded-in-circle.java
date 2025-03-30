176c0ff0226edc86cda477569abc8d4eb360d999
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-03-30 18:26:38 -0500

    Time: 0 ms (100%), Space: 41.7 MB (19.8%) - LeetHub

---
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        int x = 0;
+        int y = 0;
+        int idx = 0;
+        for (char S: instructions.toCharArray()){
+            if (S == 'G'){
+                int[] dir = directions[idx];
+                x += dir[0];
+                y += dir[1];
+            }
+            else if(S == 'L'){
+                idx = (idx + 1) % 4;
+            }
+            else{
+                idx = (idx + 3) % 4;
+            }
+        }
+
+        if ((x == 0 & y == 0) || idx != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
