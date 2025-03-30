37017e29fec16c8de65b6be3c1570a8d5c1de009
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-03-30 14:07:45 -0500

    Time: 30 ms (16.9%), Space: 46.2 MB (96.57%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,11 +1,34 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int min_val = nums[0];
+        int max_val = nums[0];
         int res = 0;
-        for(int i=0; i<n; i+=2) {
-            res += nums[i];
-        }  
+        //Bucket Sort
+        for (int i=0; i < n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            min_val = Math.min(min_val, nums[i]);
+            max_val = Math.max(max_val, nums[i]);
+        }
+        Boolean Flag = false;
+        for (int i = min_val; i <= max_val; i++){
+            if (map.containsKey(i)){
+                if (Flag){
+                    map.put(i, map.get(i)-1);
+                }
+                int freq = map.get(i);
+                if (freq % 2 == 0){
+                    res += (freq/2) * i;
+                    Flag = false; // even
+                }
+                else{
+                    res += (freq-1)/2 * i;
+                    res += i;
+                    Flag = true; //odd
+                }
+            }
+        }
         return res;
     }
 }
