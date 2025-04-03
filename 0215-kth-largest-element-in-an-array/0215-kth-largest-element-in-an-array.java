134e2709e5e53ddb6d3a96b5de0306211729fea7
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-04-03 16:31:38 -0500

    Time: 62 ms (43.39%), Space: 61.6 MB (30.31%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,31 +1,31 @@
 // MinHeap: Time: O(Nlogk); Space: O(k)
-// class Solution {
-//     public int findKthLargest(int[] nums, int k) {
-//         PriorityQueue<Integer> heap = new PriorityQueue<>();
-//         for(int num:nums){
-//             heap.offer(num);
-
-//             if (heap.size() > k){
-//                 heap.poll();
-//             }
-//         }
-
-//         return heap.peek();
-//     }
-// }
-
-// MaxHeap: Time: O(N) + O(klogN); Space: O(N)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> max_heap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
         for(int num:nums){
-            max_heap.offer(num);
+            heap.offer(num);
+
+            if (heap.size() > k){
+                heap.poll();
+            }
         }
 
-        for(int i=0; i < k-1; i++){
-            max_heap.poll();
-        }
-
-        return max_heap.peek();
+        return heap.peek();
     }
 }
+
+// MaxHeap: Time: O(N) + O(klogN); Space: O(N)
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> max_heap = new PriorityQueue<>(Collections.reverseOrder());
+//         for(int num:nums){
+//             max_heap.offer(num);
+//         }
+
+//         for(int i=0; i < k-1; i++){
+//             max_heap.poll();
+//         }
+
+//         return max_heap.peek();
+//     }
+// }
