f2a625272a169755f2f795fa0e19162b54553b86
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-04-03 16:31:25 -0500

    Time: 71 ms (19.15%), Space: 57.4 MB (77.45%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,15 +1,31 @@
-// MinHeap: 
+// MinHeap: Time: O(Nlogk); Space: O(k)
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> heap = new PriorityQueue<>();
+//         for(int num:nums){
+//             heap.offer(num);
+
+//             if (heap.size() > k){
+//                 heap.poll();
+//             }
+//         }
+
+//         return heap.peek();
+//     }
+// }
+
+// MaxHeap: Time: O(N) + O(klogN); Space: O(N)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        PriorityQueue<Integer> max_heap = new PriorityQueue<>(Collections.reverseOrder());
         for(int num:nums){
-            heap.offer(num);
-
-            if (heap.size() > k){
-                heap.poll();
-            }
+            max_heap.offer(num);
         }
 
-        return heap.peek();
+        for(int i=0; i < k-1; i++){
+            max_heap.poll();
+        }
+
+        return max_heap.peek();
     }
 }
