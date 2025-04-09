0c1f06181fe11e9c511b8f858eaff8f3daa8db2f
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-04-09 02:06:00 -0500

    Time: 7 ms (69.37%), Space: 18.1 MB (24.55%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,7 +1,10 @@
+# Approach: Stack; Best solution
+# Time: O(n); Space: O(n)(res) + O(n/2)(For Stack as we append only start times)
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         res = [0] * n
         mystack = []
+        # Will be different for start time and end time
         last_execution = 0
         
         for log in logs:
@@ -10,12 +13,18 @@ class Solution:
             time = int(time)
 
             if typ == "start":
+                # only if previous threads exists
                 if mystack:
+                    # update the execution time into the res
                     res[mystack[-1]] += time - last_execution
+                # push to stack the current executed thread
                 mystack.append(thread_id)
+                # Since its a start thread last_execution will be updated with latest time for next thread
                 last_execution = time
             else:  # end
+                # "+1" as we count the end time as extra time # imp step
                 res[mystack.pop()] += time - last_execution + 1
+                # last_execution for next thread should start from next minute or second 
                 last_execution = time + 1
         
         return res
