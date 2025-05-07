3a3262a2dd44faf3a554df81838f798bf3c28036
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-07 18:08:46 -0500

    Time: 43 ms (84.32%), Space: 34.5 MB (43.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,52 +1,19 @@
-# More efficient than the below one as we are not creating a new list everytime
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        
-        def helper(s, path):
-            if not s:
+        n = len(s)
+
+        def helper(path, pivot):
+            if pivot == n:
                 res.append(path[:])
                 return
-            
-            for i in range(1,len(s)+1):
-                partition = s[:i]
-                if ispalindrome(partition):
-                    #path = path + [partition]
-                    path.append(partition)
-                    helper(s[i:],path)
-                    path.pop()
-                
 
-        
-        def ispalindrome(path):
-            l = 0
-            r = len(path) -1
-            while l < r:
-                if path[l] != path[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        
-        helper(s, [])
+            for i in range(pivot, n):
+                partition = s[pivot:i+1]
+                if partition == partition[::-1]:
+                    path.append(partition)
+                    helper(path,i+1)
+                    path.pop()
+
+        helper([],0)
         return res
-    
-# class Solution:
-#     def partition(self, s: str) -> List[List[str]]:
-#         res = []
-        
-#         def helper(s, path):
-#             if not s:
-#                 res.append(path)
-#                 return
-            
-#             for i in range(1, len(s) + 1):
-#                 partition = s[:i]
-#                 if is_palindrome(partition):
-#                     helper(s[i:], path + [partition])
-        
-#         def is_palindrome(sub):
-#             return sub == sub[::-1]  # Simple palindrome check
-        
-#         helper(s, [])
-#         return res
