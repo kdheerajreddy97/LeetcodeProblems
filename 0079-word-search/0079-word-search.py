657f098b39efad5bea8dab12c445620d6d899b60
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-08 18:58:17 -0500

    Time: 4276 ms (56.11%), Space: 17.9 MB (39.91%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,45 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.m = len(board)
-        self.n = len(board[0])
-        self.board = board
-        
-        
-        for i in range(self.m):
-            for j in range(self.n):
-                if self.helper(i, j, word):
-                    return True
-        return False
-        
-        
-    def helper(self, i, j, suffix):
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
-        # Base Case
-        if len(suffix) == 0:
-            return True
-            
-        #Check bounds
-        if i < 0 or i >= self.m or j < 0 or j >= self.n or self.board[i][j] != suffix[0]:
-            return False
-            
-        temp, self.board[i][j] = self.board[i][j], "#"
-            
-        #Recurse
-        for x, y in directions:
-            if self.helper(i+x, j+y, suffix[1:]):
+        m = len(board)
+        n = len(board[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+
+        def helper(i, j , pivot):
+
+            if pivot == len(word):
                 return True
-                
-        self.board[i][j] = suffix[0]
-        self.board[i][j] = temp
+
+            if i < 0 or i > m-1 or j < 0 or j > n-1 or board[i][j] != word[pivot]:
+                return False
+            
+            board[i][j] = '#'
+
+            for dir in directions:
+                r = i + dir[0]
+                c = j + dir[1]
+                if helper(r, c, pivot + 1):
+                    return True
+            board[i][j] = word[pivot]
+
+
+        for i in range(m):
+            for j in range(n):
+                if helper(i, j, 0):
+                    return True
+
         return False
-            
-            
-                
-                
-                
-                
-                
-            
-            
-            
-            
