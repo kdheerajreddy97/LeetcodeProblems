6b01e8dc34ede5a7f1c9e94e811a713e2d1ad708
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-11 02:24:31 -0500

    Time: 833 ms (37.89%), Space: 18 MB (87.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,20 +2,17 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         m = len(coins)
         n = amount
-        dp = [[amount+1] * (n+1) for _ in range(m+1)]
+        dp = [amount+1] * (n+1)
 
-        for i in range(m+1):
-            dp[i][0] = 0
+        dp[0] = 0
 
         for i in range(1,m+1):
             for j in range(1, n+1):
-                if j < coins[i-1]:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i-1]])
+                if (j >= coins[i-1]):
+                    dp[j] = min(dp[j], 1 + dp[j-coins[i-1]])
 
-        if dp[m][n] != (amount + 1):
-            return dp[m][n]
+        if dp[n] != (amount + 1):
+            return dp[n]
         else:
             return -1
                 
