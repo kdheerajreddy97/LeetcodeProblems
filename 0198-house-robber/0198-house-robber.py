5a2b14a1a4a9f658c23fb487ce7542d75b06616b
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-13 17:34:41 -0500

    Time: 0 ms (100%), Space: 17.8 MB (54.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,16 @@
+# Only one decision maming parameter
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        m = len(nums)
-        
-        if m == 0:
+        n = len(nums)
+        if n == 0:
             return 0
-        if m == 1:
-            return nums[0]
-        dp = [0] * m
-        dp[0] = nums[0]    
-        dp[1] = max(nums[1],nums[0])
-        
-        for i in range(2,m):
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-        
-        
-        return dp[m-1]
+        if n == 1:
+             return nums[0]
+
+        dp = [0] * (n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2,n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[n-1]
