d6ff2c127873a4ace91069c2285b17da0129a16c
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-14 01:26:24 -0500

    Time: 3 ms (72.23%), Space: 17.8 MB (64.92%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -1,22 +1,20 @@
+# Greedy -> Exhaustive -> If Repeated subproblems -> DP;
+# Tabulation: Time: O(m*n); Space: O(m*n)
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        m = len(costs) 
+        m = len(costs)
         n = len(costs[0])
-        
-        if not costs:
-            return 0
-        
-        dp = [[0]*n for _ in range(m)]
-        
+
+        dp = [[0] * n for _ in range(m)]
+        # Fill the last row
         for j in range(n):
             dp[m-1][j] = costs[m-1][j]
-            
-        
+
+        # Start filling the dp 
         for i in range(m-2, -1, -1):
+            # add current cost to min of below row
             dp[i][0] = costs[i][0] + min(dp[i+1][1], dp[i+1][2])
             dp[i][1] = costs[i][1] + min(dp[i+1][0], dp[i+1][2])
             dp[i][2] = costs[i][2] + min(dp[i+1][0], dp[i+1][1])
-            
         
-        return min(dp[0][0],dp[0][1],dp[0][2])
-    
+        return min(dp[0][0], dp[0][1], dp[0][2])
