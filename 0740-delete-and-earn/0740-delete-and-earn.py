0807fa0c23cc79de455945fb64239fbfc9325c43
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-14 17:42:39 -0500

    Time: 7 ms (80.24%), Space: 19.3 MB (88.51%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,48 +1,19 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        
-        max_point = {}
-        
-        if len(nums) == 1:
-            return nums[0]
-        
-        for i in nums:
-            if i in max_point:
-                max_point[i] += i
-            else:
-                max_point[i] = i
-        
-        
-        max_length = max(max_point)
-        points = []
-        
-        for i in range(max_length+1):
-            if i in max_point:
-                points.append(max_point[i])
-            else:
-                points.append(0)
-        print(points)
-        
-        
-        k = len(points)
-        dp = [0] * k
-        
-        dp[0] = points[0]
-        dp[1] = max(points[0],points[1])
-        
-        for i in range(2, k):
-            dp[i] = max(points[i] + dp[i - 2], dp[i-1])
-        
-        return dp[k-1]
-        
-        
-        
-#         points = [0] * max_length
-#         print(points)
-        
-#         for key, val in max_point.items():
-#             points[key] = val
+        n = len(nums)
+        max_number = max(nums)
+        points = [0] * (max_number+1)
+
+        for num in nums:
+            points[num] += num
+        m = len(points)
+        dp = [0] * len(points)
+
+        dp[0] = 0
+        dp[1] = points[1]
+
+        for i in range(2,len(points)):
+            dp[i] = max(dp[i-1], points[i] + dp[i-2])
+
+        return dp[m-1]
             
-#         print(points)
-            
-        
