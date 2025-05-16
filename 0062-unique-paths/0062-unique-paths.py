966f47c6f2dc11919d0f88b64b1f9cb3bfcf7e8c
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-15 21:32:57 -0500

    Time: 0 ms (100%), Space: 18 MB (10.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,14 @@
-# Normal recursive approach; Time complexity : 2^(m+n)
+# Bottom-up DP; Time complexity: O(m*n); Space: O(m*n)
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = [[0] * n for _ in range(m)]
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        dp[m-1][n-1] = 1
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                # Edge condition
+                if (i == m-1 and j == n-1):
+                    continue
+                # as per question
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
 
-        def helper(i,j):
-            if (i == m-1 and j == n-1):
-                return 1
-            if (i >= m or j >=n):
-                return 0
-            if memo[i][j] != 0:
-                return memo[i][j]
-                
-            cost1 = helper(i, j+1)
-            cost2 = helper(i+1, j)
-
-            memo[i][j] = cost1 + cost2
-
-            return memo[i][j]
-
-        return helper(0,0)
+        return dp[0][0]
