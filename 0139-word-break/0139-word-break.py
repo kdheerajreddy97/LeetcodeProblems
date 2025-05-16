228ed441490523c2fba5f917262d08fc0ba7c00a
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-16 14:12:23 -0500

    Time: 19 ms (6.14%), Space: 18.2 MB (12.17%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,14 +1,22 @@
+# Dp - Memoization
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s)+1)
-        dp[len(s)] = True
-        
-        for i in range(len(s) -1,  -1, -1):
-            for w in wordDict:
-                if i + len(w) <= len(s) and s[i:i+len(w)] == w: 
-                        dp[i] = dp[i + len(w)]
-                if dp[i]:
-                    break
-        
-        return dp[0]
-        
+        wordset = set(wordDict)
+        n = len(s)
+        memo = set() # using memo set instead of hashmap
+        def helper(sub):
+            if len(sub) == 0:
+                return True
+            if sub in memo:
+                return False
+
+            for i in range(len(sub)):
+                substr = sub[0: i+1]
+                print(substr)
+                if(substr in wordset) and (helper(sub[i+1:])):
+                    return True
+
+            memo.add(sub)
+            return False
+
+        return helper(s)
