c3cb761e241174aa42a84f4de5617f142a2ec095
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-19 17:56:53 -0500

    Time: 0 ms (100%), Space: 24.9 MB (97.67%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.py
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.py
@@ -5,37 +5,36 @@
 #         self.left = left
 #         self.right = right
 # Approach1: BFS level order traversal: Time: O(N); Space: O(N)
-# class Solution:
-#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        # q = deque([root])
-        # sum = 0
-        # while q:
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         if low <= node.val <= high:
-        #             sum += node.val
-        #         # Igoring sub trees not following this condition
-        #         if node.val > low and node.left:
-        #             q.append(node.left)
-        #         if node.val < high and node.right:
-        #             q.append(node.right)
-        # return sum
-
-# Approach1: DFS Preorder or Postorder or Inorder: Time: O(N); Space: O(N)
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        def dfs(node):
-            total_sum = 0
-            if not node:
-                 return 0
-            if low <= node.val <= high:
-                total_sum += node.val
+        q = deque([root])
+        sum = 0
+        while q:
+                node = q.popleft()
+                if low <= node.val <= high:
+                    sum += node.val
+                # Igoring sub trees not following this condition
+                if node.val > low and node.left:
+                    q.append(node.left)
+                if node.val < high and node.right:
+                    q.append(node.right)
+        return sum
+
+# Approach1: DFS Preorder or Postorder or Inorder: Time: O(N); Space: O(N)
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         def dfs(node):
+#             total_sum = 0
+#             if not node:
+#                  return 0
+#             if low <= node.val <= high:
+#                 total_sum += node.val
             
-            if node.val > low:
-                total_sum += dfs(node.left)
-            if node.val < high:
-                total_sum += dfs(node.right)
+#             if node.val > low:
+#                 total_sum += dfs(node.left)
+#             if node.val < high:
+#                 total_sum += dfs(node.right)
             
-            return total_sum
+#             return total_sum
         
-        return dfs(root)
+#         return dfs(root)
