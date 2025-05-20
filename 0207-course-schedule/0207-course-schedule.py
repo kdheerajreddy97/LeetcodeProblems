8d3e4c1185adcfa1285f8a165016450abff28d88
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2025-05-19 23:15:49 -0500

    Time: 8 ms (24.92%), Space: 19.5 MB (30.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,8 +1,12 @@
+#Approch: DFS traversal; Time: O(V+E); Space: O(V)+ O(V)+ O(V+E)(visited set + recursive stack space + adj list) = O(V+E)
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        #Create Dicttionary with courses and empty lists(prereq)
-        preMap = {i:[] for i in range(numCourses)}
+        #Create Dictionary with courses and empty lists(prereq)
+        # preMap = {i:[] for i in range(numCourses)}
+        preMap = defaultdict(list)
+        # for i in range(numCourses):
+        #     preMap[i] = []
         
         for course, prereq in prerequisites:
             preMap[course].append(prereq) # This dictionary contains the courses and its prerequisites as Key values pair
@@ -10,8 +14,9 @@ class Solution:
         # visited set
         visit = set()
         
-        #implementing dfs
+        #implementing dfs; basically detecting cycle
         def dfs(crs):
+            # if in visted then there is cycle as we are visting the same node
             if crs in visit:
                 return False
             if preMap[crs] == []:
@@ -22,11 +27,12 @@ class Solution:
             for prereq in preMap[crs]:
                 if not dfs(prereq):
                     return False
+            # Make it empty; since no cycle
             preMap[crs] = []
             visit.remove(crs)
             return True
         
-        
+        # There might individual different graphs; hence we need to run dfs on every node
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
